b915fc594c5dd140724c89e842c743b937d66e61
Raşit Gürses <106206404+rasitgurses@users.noreply.github.com>
2023-10-08 13:58:06 +0300

    Add files via upload

---
+++ W4CaseStd2.py
@@ -0,0 +1,160 @@
+#####################################################
+# AB Testi ile BiddingYöntemlerinin Dönüşümünün Karşılaştırılması
+#####################################################
+
+#####################################################
+# İş Problemi
+#####################################################
+
+# Facebook kısa süre önce mevcut "maximumbidding" adı verilen teklif verme türüne alternatif
+# olarak yeni bir teklif türü olan "average bidding"’i tanıttı. Müşterilerimizden biri olan bombabomba.com,
+# bu yeni özelliği test etmeye karar verdi ve averagebidding'in maximumbidding'den daha fazla dönüşüm
+# getirip getirmediğini anlamak için bir A/B testi yapmak istiyor.A/B testi 1 aydır devam ediyor ve
+# bombabomba.com şimdi sizden bu A/B testinin sonuçlarını analiz etmenizi bekliyor.Bombabomba.com için
+# nihai başarı ölçütü Purchase'dır. Bu nedenle, istatistiksel testler için Purchase metriğine odaklanılmalıdır.
+
+
+
+
+#####################################################
+# Veri Seti Hikayesi
+#####################################################
+
+# Bir firmanın web site bilgilerini içeren bu veri setinde kullanıcıların gördükleri ve tıkladıkları
+# reklam sayıları gibi bilgilerin yanı sıra buradan gelen kazanç bilgileri yer almaktadır.Kontrol ve Test
+# grubu olmak üzere iki ayrı veri seti vardır. Bu veri setleri ab_testing.xlsxexcel’ininayrı sayfalarında yer
+# almaktadır. Kontrol grubuna Maximum Bidding, test grubuna AverageBiddinguygulanmıştır.
+
+# impression: Reklam görüntüleme sayısı
+# Click: Görüntülenen reklama tıklama sayısı
+# Purchase: Tıklanan reklamlar sonrası satın alınan ürün sayısı
+# Earning: Satın alınan ürünler sonrası elde edilen kazanç
+
+
+
+#####################################################
+# Proje Görevleri
+#####################################################
+
+
+######################################################
+# AB Testing (Bağımsız İki Örneklem T Testi)
+######################################################
+
+# 1. Hipotezleri Kur
+# 2. Varsayım Kontrolü
+#   - 1. Normallik Varsayımı (shapiro)
+#   - 2. Varyans Homojenliği (levene)
+# 3. Hipotezin Uygulanması
+#   - 1. Varsayımlar sağlanıyorsa bağımsız iki örneklem t testi
+#   - 2. Varsayımlar sağlanmıyorsa mannwhitneyu testi
+# 4. p-value değerine göre sonuçları yorumla
+# Not:
+# - Normallik sağlanmıyorsa direkt 2 numara. Varyans homojenliği sağlanmıyorsa 1 numaraya arguman girilir.
+# - Normallik incelemesi öncesi aykırı değer incelemesi ve düzeltmesi yapmak faydalı olabilir.
+
+
+import pandas as pd
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# !pip install statsmodels
+import statsmodels.stats.api as sms
+from scipy.stats import ttest_1samp, shapiro, levene, ttest_ind, mannwhitneyu, \
+    pearsonr, spearmanr, kendalltau, f_oneway, kruskal
+from statsmodels.stats.proportion import proportions_ztest
+
+
+
+#####################################################
+# Görev 1:  Veriyi Hazırlama ve Analiz Etme
+#####################################################
+
+# Adım 1:  ab_testing_data.xlsx adlı kontrol ve test grubu verilerinden oluşan veri setini okutunuz. Kontrol ve test grubu verilerini ayrı değişkenlere atayınız.
+
+df1 = pd.read_excel('ab_testing.xlsx',sheet_name= 'Control Group')
+df2 = pd.read_excel('ab_testing.xlsx',sheet_name= 'Test Group')
+
+# Adım 2: Kontrol ve test grubu verilerini analiz ediniz.
+def check_df(dataframe, head=10):
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### Isnull #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Describe #####################")
+    print(dataframe.describe().T)
+
+check_df(df1)
+
+check_df(df2)
+
+
+
+
+
+# Adım 3: Analiz işleminden sonra concat metodunu kullanarak kontrol ve test grubu verilerini birleştiriniz.
+df = pd.concat([df1, df2], ignore_index=True)
+
+
+
+
+
+#####################################################
+# Görev 2:  A/B Testinin Hipotezinin Tanımlanması
+#####################################################
+
+# Adım 1: Hipotezi tanımlayınız.
+
+# H0: M1 = M2(kontrol ve test grubu arasında istatiksel olarak dön. fark. yoktur)
+# H1: M1 != M2
+
+# Adım 2: Kontrol ve test grubu için purchase(kazanç) ortalamalarını analiz ediniz
+df1['Purchase'].mean()
+€df2['Purchase'].mean()
+#####################################################
+# GÖREV 3: Hipotez Testinin Gerçekleştirilmesi
+#####################################################
+
+######################################################
+# AB Testing (Bağımsız İki Örneklem T Testi)
+######################################################
+df1.head()
+
+# Adım 1: Hipotez testi yapılmadan önce varsayım kontrollerini yapınız.Bunlar Normallik Varsayımı ve Varyans Homojenliğidir.
+# Kontrol ve test grubunun normallik varsayımına uyup uymadığını Purchase değişkeni üzerinden ayrı ayrı test ediniz
+
+test_stat, pvalue = shapiro(df1["Purchase"])
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+
+test_stat, pvalue = shapiro(df2["Purchase"])
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+
+#varyans homojenliği
+# Varyans homojenliği
+# H0: Varyanslar Homojendir
+# H1: Varyanslar Homojen Değildir
+test_stat, pvalue = levene(df1["Purchase"],
+                           df2["Purchase"])
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+
+# Adım 2: Normallik Varsayımı ve Varyans Homojenliği sonuçlarına göre uygun testi seçiniz
+
+test_stat, pvalue = ttest_ind(df1["Purchase"],
+                              df2["Purchase"],
+                              equal_var=True)
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+
+
+# Adım 3: Test sonucunda elde edilen p_value değerini göz önünde bulundurarak kontrol ve test grubu satın alma
+# ortalamaları arasında istatistiki olarak anlamlı bir fark olup olmadığını yorumlayınız.
+
+
+
+
